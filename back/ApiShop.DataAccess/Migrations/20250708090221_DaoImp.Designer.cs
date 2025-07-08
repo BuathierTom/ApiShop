6f9e96c99a2f36d79ecaafd0d7a00f4196f9a2fe
buathier.tom <buathier_tom@orange.fr>
2025-07-08 11:04:21 +0200

    chore: migration update

--- back/ApiShop.DataAccess/Migrations/20250708090221_DaoImp.Designer.cs
+++ back/ApiShop.DataAccess/Migrations/20250708090221_DaoImp.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using ApiShop.DataAccess;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace ApiShop.DataAccess.Migrations
 {
     [DbContext(typeof(ApiShopDbContext))]
-    partial class ApiShopDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250708090221_DaoImp")]
+    partial class DaoImp
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
