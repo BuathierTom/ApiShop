31732835f9776a3514171d55c7cdd49adb02d5a8
buathier.tom <buathier_tom@orange.fr>
2025-07-08 12:18:01 +0200

    feat: home and link css

---
+++ front/src/pages/HomePage.tsx
@@ -0,0 +1,14 @@
+import Header from '../components/Header';
+
+const HomePage = () => {
+  return (
+    <>
+      <Header />
+      <div className="p-4">
+        <h1 className="text-2xl font-bold">Bienvenue sur ApiShop !</h1>
+      </div>
+    </>
+  );
+};
+
+export default HomePage;
