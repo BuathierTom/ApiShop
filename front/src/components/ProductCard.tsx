14478962955e1873cb43d23814c368ca7f7efb48
buathier.tom <buathier_tom@orange.fr>
2025-07-08 14:42:46 +0200

    feat: add product to cart with api

--- front/src/components/ProductCard.tsx
+++ front/src/components/ProductCard.tsx
@@ -1,7 +1,19 @@
 import type { ProductDto } from '../types/Product';
+import { useCart } from '../context/CartContext';
+import { toast } from 'react-toastify';
 
 const ProductCard = ({ product }: { product: ProductDto }) => {
+  const { addToCart } = useCart();
   const placeholder = 'https://placehold.co/300x200?text=Image+non+disponible';
+
+  const handleAddToCart = async () => {
+    try {
+      await addToCart(product);
+      toast.success(`${product.name} ajouté au panier`);
+    } catch (error) {
+      toast.error("Une erreur est survenue");
+    }
+  };
 
   return (
     <div className="border rounded-lg p-4 shadow hover:shadow-lg transition">
@@ -15,7 +27,10 @@ const ProductCard = ({ product }: { product: ProductDto }) => {
       />
       <h2 className="font-semibold text-lg">{product.name}</h2>
       <p className="text-gray-500 mb-1">{product.price.toFixed(2)} €</p>
-      <button className="w-full bg-blue-600 text-white py-2 rounded hover:bg-blue-700 mt-2">
+      <button
+        onClick={handleAddToCart}
+        className="w-full bg-blue-600 text-white py-2 rounded hover:bg-blue-700 mt-2 cursor-pointer"
+      >
         Ajouter au panier
       </button>
     </div>
