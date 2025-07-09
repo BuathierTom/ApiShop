3a833270b790ab4febffc20f85f1ab68e2e281bb
buathier.tom <buathier_tom@orange.fr>
2025-07-09 10:50:43 +0200

    fix: Link never use

--- front/src/components/Header.tsx
+++ front/src/components/Header.tsx
@@ -1,4 +1,4 @@
-import { NavLink, Link, useNavigate } from 'react-router-dom';
+import { NavLink, useNavigate } from 'react-router-dom';
 import { useAuth } from '../context/AuthContext';
 import { ShoppingCart, User } from 'lucide-react';
 import { useState } from 'react';
